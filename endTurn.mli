877e602e3d7abb80046d04dc4719db749a6782d4
ycwuaa <ycwuaa@stu.ust.hk>
2015-11-07 14:26:04 -0500

    hide type in gameState and change of init.mli and endTurn.mli

--- endTurn.mli
+++ endTurn.mli
@@ -1,5 +1,7 @@
 (** endTurn.mli *)
 
+open GameState
+
 (** move the army from territory to territory
   * NOTICE: territory on the way all need to be the current player's
   * take in the playerid, from territory to territory and return whether the
@@ -8,10 +10,10 @@ val move_army int -> territory -> territory -> bool
 
 (** check whether any player win the game or not at the end of this turn
   * if no one win yet, return None else give bakc the int option contiaining
-  * the winning players_id*)
-val check_win unit -> int option
+  * the winning player_id*)
+val check_win unit -> player_id option
 
 (** check whether any player is out at the end of this turn
   * if no one is out, give back None else give back all the playerid of who
   * already lost in a int list option*)
-val check_defeated unit -> int list option
+val check_defeated unit -> player_id list option
