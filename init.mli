2fe278931293b99237517719f68ec9e54f9bc838
ycwuaa <ycwuaa@stu.ust.hk>
2015-11-07 13:23:54 -0500

    init.mli and endTurn.mli edited

--- init.mli
+++ init.mli
@@ -1,1 +1,13 @@
 (** init.mli *)
+
+(** according to the number of player, distribute the infantry*)
+val distribute_army unit -> unit
+
+(** distribute the terriyories randomly to all the players*)
+val distribute_territory unit -> unit
+
+(** place the army as the player indicates : who(palyerid), how many and where*)
+val place_army int -> int -> territory -> unit
+
+(** decide which player to start from by taking in the player id*)
+val set_first_player int -> unit
