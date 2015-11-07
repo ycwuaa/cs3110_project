38d0a48314462d98e21d79dd090abc4ae04c0850
tomchm <tomchm1@gmail.com>
2015-11-07 16:00:12 -0500

    Adding input.mli

--- input.mli
+++ input.mli
@@ -1,1 +1,25 @@
 (** input.mli *)
+(** Handles all game actions by human players*)
+
+(** Gets the player name at the beginning of the game.*)
+val choose_name : unit -> string
+
+(** takes in a GameState and the number of extra armies received, and returns
+ * a new GameState containing the newly added armies that the human had
+ * added onto the territories *)
+val place_new_armies : t -> int -> t
+
+(** takes in a GameState and returns Some (from, to, dice) if player will
+ * attack the [to] territory with the army on the [from] territory using [dice]
+ * number of dice, or None if no attack is desired *)
+val choose_attack : t -> (territory * territory * int) option
+
+(** takes in a GameState, a territory to move from, a territory to move to, and
+ * the minimum number of armies to move; returns the chosen number of armies to
+ * move *)
+val choose_move_conquerors : t -> territory -> territory -> int -> int
+
+(** takes in a GameState and returns Some (from, to, num) if player moves
+ * [num] armies from the [from] territory to the [to] territory, or None if
+ * no redistribution is desired *)
+val redistribute_armies : t -> int -> (territory * territory * int) option
