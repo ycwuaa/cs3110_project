76ec3c7571babdfd9797860a579c8ca641879fa6
Derrick <drl222@cornell.edu>
2015-11-07 15:22:58 -0500

    update AI.mli

--- AI.mli
+++ AI.mli
@@ -7,10 +7,15 @@ open GameState
  * each territory *)
 val place_new_armies : t -> int -> (territory * int) list
 
-(** takes in a GameState and returns Some (from, to) if the AI wants to attack
- * the [to] territory with the army on the [from] territory, or None if no
- * attack is desired *)
-val choose_attack : t -> (territory * territory) option
+(** takes in a GameState and returns Some (from, to, dice) if the AI wants to
+ * attack the [to] territory with the army on the [from] territory using [dice]
+ * number of dice, or None if no attack is desired *)
+val choose_attack : t -> (territory * territory * int) option
+
+(** takes in a GameState, a territory to move from, a territory to move to, and
+ * the minimum number of armies to move; returns the chosen number of armies to
+ * move *)
+val choose_move_conquerors : t -> territory -> territory -> int -> int
 
 (** takes in a GameState and returns Some (from, to, num) if the AI wants to
  * [num] armies from the [from] territory to the [to] territory, or None if
