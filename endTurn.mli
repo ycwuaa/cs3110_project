3a0160022b09161985f8167d613871d1ca47f70f
ycwuaa <ycwuaa@stu.ust.hk>
2015-11-11 17:44:51 -0500

    edited init and endTurn

--- endTurn.mli
+++ endTurn.mli
@@ -4,16 +4,15 @@ open GameState
 
 (** move the army from territory to territory
   * NOTICE: territory on the way all need to be the current player's
-  * take in the playerid, from territory to territory and return whether the
-  * move is successful or not*)
-val move_army : t -> int -> territory -> territory -> bool
+  * take in the playerid, from territory to territory*)
+val move_army : t -> int -> territory -> territory -> t
 
 (** check whether any player win the game or not at the end of this turn
   * if no one win yet, return None else give bakc the int option contiaining
   * the winning player_id*)
-val check_win : t -> unit -> player_id option
+val check_win : t -> player_id option
 
 (** check whether any player is out at the end of this turn
-  * if no one is out, give back None else give back all the playerid of who
+  * if no one is out, give back empty list else give back all the playerid of who
   * already lost in a int list option*)
-val check_defeated : t -> unit -> player_id list option
+val check_defeated : t -> player_id list
--- init.mli
+++ init.mli
@@ -2,14 +2,11 @@
 
 open GameState
 
-(** according to the number of player, distribute the infantry*)
-val distribute_army : t -> unit -> unit
-
 (** distribute the terriyories randomly to all the players*)
-val distribute_territory : t -> unit -> unit
+val distribute_territory : t -> t
 
 (** place the army as the player indicates : who(player_id), how many and where*)
-val place_army : t -> player_id -> int -> territory -> unit
+val place_army : t -> player_id -> (int*territory) list -> t
 
 (** decide which player to start from by taking in the player id*)
-val set_first_player : t -> player_id -> unit
+val set_first_player : t -> t
