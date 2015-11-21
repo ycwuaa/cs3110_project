2fadcb3dad3b7b004373f8b7a120a74a10d31d63
Rebecca Bernstein <reb343@cornell.edu>
2015-11-20 19:43:42 -0500

    fixed syntax error in begin mli, began implementation of beginturn

--- beginTurn.mli
+++ beginTurn.mli
@@ -1,13 +1,13 @@
 (** beginTurn.mli : ***********************************************************)
 (** handles distributing army pieces and placing them on the map **************)
 
-Open GameState
+open GameState
 
 (** returns number of pieces to award to player with player_id who has control
   * of territories in the game state; checks for continent control and awards
   * accordingly*)
-val award_pieces : t -> player_id -> int;
+val award_pieces : t -> player_id -> int
 
 (** returns a game state with n pieces aligned with player_id placed on given
   * territory*)
-val place_piece : t -> int -> player_id -> territory -> t;
+val place_piece : t -> int -> player_id -> territory -> t
