164ec64e8f16525044915ca22365fa6852ac214f
ycwuaa <ycwuaa@stu.ust.hk>
2015-11-21 15:59:28 -0500

    endTurn.ml added

--- endTurn.mli
+++ endTurn.mli
@@ -5,10 +5,10 @@ open GameState
 (** move the army from territory to territory
   * NOTICE: territory on the way all need to be the current player's
   * take in the playerid, from territory to territory*)
-val move_army : t -> int -> territory -> territory -> t
+val move_army : t -> player_id -> int -> territory -> territory -> t
 
 (** check whether any player has won the game or not at the end of this turn
-  * if no one win yet, return None else give bakc the int option contiaining
+  * if no one win yet, return None else give back the int option contiaining
   * the winning player_id*)
 val check_win : t -> player_id option
 
