f199322382226a314c2fae354a73083fc90d673b
Derrick <drl222@cornell.edu>
2015-12-01 00:54:43 -0500

    finished coding a dumb (but legal) AI

--- AI.ml
+++ AI.ml
@@ -2,6 +2,16 @@
 
 open GameState
 
+
+(* helper debug function to print out a territory list *)
+let rec print_list tostring = function
+| [] -> ()
+| h::t ->
+  let _ = Printf.printf " @ %s\n" (tostring h) in
+  print_list tostring t
+
+
+
 let choose_name =
   let curr_int = ref 1 in
   fun () ->
@@ -9,49 +19,63 @@ let choose_name =
     let _ = incr curr_int in
     returnVal
 
+(* current strategy: place equally everywhere *)
 let place_original_armies (gs:t) (num_new:int) =
-  failwith "place_original_armies unimplemented"
-
-let place_new_armies (gs:t) (num_new:int) =
   let p' = get_active_player gs in
-  (* place 2/3 of current armies on the first known territory *)
   let my_terr = get_territories gs p' in
-  let _ = Printf.printf "%s Territory %s\n" (string_of_territory gs (List.hd my_terr)) in
-  ((if num_new > 1 then (num_new * 2 / 3) else 1), List.hd my_terr)
+  let num_terr = List.length my_terr in
 
-let choose_attack (gs:t) =
-  let p' = get_active_player gs in
-  if Attack.roll_dice () <= 1 then
-    None
-  else
-    let my_terr = get_territories gs p' in
-    let usable_terr = List.filter (fun terr -> get_armies gs terr > 1) my_terr in
-
-    (*let rec print_terr_list = function
-    | [] -> ()
-    | h::t ->
-      let _ = Printf.printf "the territory %s\n" (string_of_territory gs h) in
-      print_terr_list t
+  (* divide up the [dividend] into a list of length [divisor] *)
+  let rec divide_up dividend divisor acc =
+    if dividend <= 0 then
+      acc
+    else if dividend < divisor then
+      dividend::acc
+    else
+      let quotient = dividend / divisor in
+      divide_up (dividend - quotient) (divisor - 1) (quotient::acc)
     in
 
-    let _ = print_terr_list usable_terr in*)
+  let number_list = divide_up num_new num_terr [] in
+  List.combine number_list my_terr
 
-    match usable_terr with
-    | [] -> None
-    | h::t ->
-        let neighbors = get_adjacency gs h in
-        match neighbors with
-        | [] -> None
-        | h::_ -> Some (h, List.hd neighbors, 3) (* TODO: make sure it's an enemy territory *)
+(* current strategy: place everything on the first territory found *)
+let place_new_armies (gs:t) (num_new:int) =
+  let p' = get_active_player gs in
+  let my_terr = get_territories gs p' in
+  (num_new, List.hd my_terr)
 
+(* current strategy: keep attacking from the first territory found with
+ * armies > 1 to the first enemy territory found that neighbors it, or
+ * do nothing if the first or second conditions are not met *)
+let choose_attack (gs:t) =
+  let p' = get_active_player gs in
+  let my_terr = get_territories gs p' in
+  let usable_terr = List.filter (fun terr -> get_armies gs terr > 1) my_terr in
 
-let choose_move_conquerors (gs:t) (terr1:territory)
-  (terr2:territory) (minMove:int) =
-  minMove
+  match usable_terr with
+  | [] -> None (* no territories with at least one army on it *)
+  | from::_ ->
+      let neighbors = get_adjacency gs from in
+      let enemy_neighbors = List.filter (fun terr -> (get_territory_owner gs terr) <> p') neighbors in
+      match enemy_neighbors with
+      | [] -> None (* our first territory is landlocked. For now, just do nothing *)
+      | toward::_ ->
+        (* roll 3 dice if possible; otherwise, roll the maximum allowed
+         * (number of armies on "from," minus 1) *)
+        let num_from = min 3 (get_armies gs from - 1) in
+        Some (from, toward, num_from)
 
+(* current strategy: move everyone possible *)
+let choose_move_conquerors (gs:t) (from:territory) (toward:territory)
+  (minMove:int) =
+  min (minMove) (get_armies gs from - 1)
+
+(* current strategy: do nothing *)
 let redistribute_armies  (gs:t) =
   None
 
-let choose_dice (gs:t) (terr1:territory) (terr2:territory) (num_enemy:int)
+(* current strategy: always defend with the most dice possible *)
+let choose_dice (gs:t) (from:territory) (toward:territory) (num_enemy:int)
   (max_dice:int) =
   max_dice
