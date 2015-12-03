d810699bcddccfa80f6f552852da6f8116382ea9
Tomek <tc464@cornell.edu>
2015-12-02 23:52:53 -0500

    Fixed distribute in Init

--- tomek.ml
+++ tomek.ml
@@ -11,26 +11,8 @@ let _ = wait_for_key ()
 
 open GameState
 open Output
+open Init
 
-(* http://stackoverflow.com/questions/15095541/
- * how-to-shuffle-list-in-on-in-ocaml *)
-let shuffle d =
-    let () = Random.self_init () in
-    let nd = List.map (fun c -> (Random.bits (), c)) d in
-    let sond = List.sort compare nd in
-    List.map snd sond
-
-let distribute_territory (gs:t) =
-  let terrs = shuffle (get_all_territories gs) in
-  let cur = get_active_player gs in
-  let give ngs terr =
-    let p = get_active_player ngs in
-    set_territory_owner ngs terr p |>
-    fun x -> set_num_armies x terr 1 |>
-    set_next_player
-  in
-  (List.fold_left give gs terrs) |>
-  fun x -> set_active_player x cur
 
 let rec wait_for_exit () =
   let a = Graphics.read_key () in
